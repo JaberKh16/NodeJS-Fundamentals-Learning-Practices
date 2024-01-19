a5aedba4979811712de1a0bf59f7178292b6e428
Jaber Khan <md.jaberkhan66@gmail.com>
2024-01-20 02:20:49 +0600

    NodeJS-Globals-Concepts

---
+++ 1.Intro-To-Globals/practices-on-global-objects/process.js
@@ -0,0 +1,23 @@
+// working with process global object
+process.stdin.on('data', (data) => {
+    process.stdout.write(`Writtten: ${data}\n`);
+    process.on('exit', () => {
+        process.exit();
+        console.log('Exited');
+    })
+})
+
+// excercise-1
+const num = Math.floor(Math.random() * 10) + 1;
+process.stdout.write('Guess a number 1-10?\n');
+process.stdin.on('data', data => {
+  let guess = data;
+  process.stdout.write(`Your guess was ${guess}`);
+  if (guess == num) {
+    process.stdout.write(`Correct you guessed ${num}\n`);
+    process.exit();
+  }
+  else {
+    process.stdout.write(`Wrong Guess of ${guess}`);
+  }
+});
