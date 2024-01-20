3d230327997e58482b1320b85ad45c72eb2b80fa
Jaber Khan <md.jaberkhan66@gmail.com>
2024-01-20 21:06:17 +0600

    Node-Built-In-Modules

--- 3.Built-In-Modules/4.http-module/practices/7.practice-7-get-form-info.js
+++ 3.Built-In-Modules/4.http-module/practices/7.practice-7-get-form-info.js
@@ -2,48 +2,66 @@ const http = require('http');
 const fs = require('fs').promises;
 const path = require('path');
 
-const setupFilePath = (path) => {
-    const filePath = path.join(__dirname, '../html', 'form.html');
-    return filePath;
+const readHtmlContent = async () => {
+    // eslint-disable-next-line no-useless-catch
+    try {
+        const filePath = path.join(__dirname, '../html', 'form.html');
+        const content = await fs.readFile(filePath, 'utf8');
+        return content;
+    } catch (err) {
+        throw err;
+    }
 };
 
-const readHtmlContent = () => {
-    const filePath = setupFilePath(path);
-    return new Promise((resolve, reject) => {
-       const content = await fs.readFile(filePath, 'utf8');
-       return content;
-    });
+const writeToFile = async (filePath, message) => {
+    try {
+        await fs.writeFile(filePath, message, 'utf8');
+    } catch (err) {
+        console.error(err);
+    }
 };
 
-const writeToFile = (path, meesage)=>{
-    return fs.writeFileSync(filePath, message);
-}
-
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     const requestUrl = req.url;
     const requestMethod = req.method;
-    if(req.url === '/'){
-        res.writeHead(200, {
-            'Content-Type': 'text/html'
-        });
-        res.write(readHtmlContent());
-        return res.end();
+
+    if (requestUrl === '/') {
+        try {
+            const content = await readHtmlContent();
+            res.writeHead(200, {
+                'Content-Type': 'text/html',
+            });
+            res.write(content);
+            return res.end();
+        } catch (err) {
+            console.error(err);
+            res.writeHead(500, {
+                'Content-Type': 'text/plain',
+            });
+            res.end('Internal Server Error');
+        }
     }
-    if (req.url === '/submitted' && req.method === 'POST') {
+
+    if (requestUrl === '/submitted' && requestMethod === 'POST') {
         const data = [];
-        req.on('data', function(chunk){
+        req.on('data', (chunk) => {
             data.push(chunk);
-            console.log(data);
-        })
-        req.on('end', function(){
-            const parsedBodyData = Buffer.concat(data).toString();
-            const mesage = parsedBodyData.split('=')[1];
-            const filePath = path.join(__dirname, '../html', 'text.txt');
-            writeToFile(filePath, mesage);
         });
+
+        req.on('end', async () => {
+            const parsedBodyData = Buffer.concat(data).toString();
+            const message = parsedBodyData.split('=')[1];
+            const filePath = path.join(__dirname, '../html', 'text.txt');
+            await writeToFile(filePath, message);
+        });
+
         res.writeHead(200, {
-            'Content-Type':'text/html'
+            'Content-Type': 'text/html',
         });
         return res.end();
     }
+});
+
+server.listen(3000, () => {
+    console.log('Server listening on port: 3000');
 });
