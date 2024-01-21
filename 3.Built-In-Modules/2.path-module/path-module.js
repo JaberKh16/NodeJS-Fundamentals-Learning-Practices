104f9829cde988b33bb3b2b582a734a4337e86c1
Jaber Khan <md.jaberkhan66@gmail.com>
2024-01-21 22:32:05 +0600

    Node-Built-In-Modules

--- 3.Built-In-Modules/2.path-module/path-module.js
+++ 3.Built-In-Modules/2.path-module/path-module.js
@@ -10,25 +10,27 @@
 
     'path' Module Methods
     ---------------------
-    a. path.join(...paths: string[]) 
-        - returns the path by joining multiple path segments. It populates 
-        all path module-related properties and methods, providing a clean 
+    a. path.join(...paths: string[])
+        - returns the path by joining multiple path segments. It populates
+        all path module-related properties and methods, providing a clean
         and cross-platform way to construct file paths.
     b. path.extname(pathfragments)
         - returns the file extension name. If specified path fragments has the
         extension then gives the extension, otherwise nothing returns.
-    c. path.basename(pathfragments) 
-        -returns the last portion of the specified path.
+    c. path.basename(pathfragments)
+        -returns the last portion of the specified path basically the current file name.
     d. path.parse(path)
-        - returns the object which represents the significant info for the 
+        - returns the object which represents the significant info for the
         specified path.
     e. path.format(path)
         - returns the path formatted.
     f. path.isAbsolute(path)
         - checks if the specified path is abosute and resulted boolean.
     g. path.resolve(path)
-        - returns the resolve of the specified path and returns the 
+        - returns the resolve of the specified path and returns the
         absolute path
+    h. path.dirname(path)
+        - returns the specified path directory name
 */
 
 const path = require('path');
