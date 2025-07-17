7df5fa8491097ce266c4ac92dc23bcaea95fda26
Jaber Khan <md.jaberkhan66@gmail.com>
2025-07-18 02:44:41 +0600

    Module-Export-Import-Concepts

--- 2.Module-Export-Imports/app.js
+++ 2.Module-Export-Imports/app.js
@@ -1,23 +1,23 @@
-const proerties_options = require('./properties-module');
+const proertiesOptions = require('./properties-module');
 const utltitiesFunction = require('./property-utitity-functions');
 
-console.log(proerties_options);
+console.log(proertiesOptions);
 
 // destructing context access
-const bugFixesArray = proerties_options.bugFixes;
+const bugFixesArray = proertiesOptions.bugFixes;
 const [feature1, feature2, feature3] = bugFixesArray;
 console.log(feature1, feature2, feature3);
 
-const feature_options = proerties_options.options;
-const { libraryName, libraryVersion, getLibraryInfo } = feature_options;
+const featureOptions = proertiesOptions.options;
+const { libraryName, libraryVersion, getLibraryInfo } = featureOptions;
 console.log(`${libraryName} ${libraryVersion} ${getLibraryInfo()}`);
 
 // property context access
-const libNameInfo = proerties_options.options.getLibraryInfo();
+const libNameInfo = proertiesOptions.options.getLibraryInfo();
 console.log(libNameInfo);
 
 // calling function - proerty context access
-const libraryInfoCall = utltitiesFunction.getLibraryInfo(proerties_options.options);
+const libraryInfoCall = utltitiesFunction.getLibraryInfo(proertiesOptions.options);
 console.log(libraryInfoCall);
 const featuresInfoCall = utltitiesFunction.newUpdateFeatures();
 console.log(featuresInfoCall);
