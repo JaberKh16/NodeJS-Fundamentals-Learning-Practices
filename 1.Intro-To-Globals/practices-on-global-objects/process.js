8bb23826de97f806c2e60d6943d8c9a28f88ec3b
Jaber Khan <md.jaberkhan66@gmail.com>
2025-07-18 01:57:56 +0600

    Node-Globals-Concepts

--- 1.Intro-To-Globals/practices-on-global-objects/process.js
+++ 1.Intro-To-Globals/practices-on-global-objects/process.js
@@ -4,20 +4,19 @@ process.stdin.on('data', (data) => {
     process.on('exit', () => {
         process.exit();
         console.log('Exited');
-    })
-})
+    });
+});
 
 // excercise-1
 const num = Math.floor(Math.random() * 10) + 1;
 process.stdout.write('Guess a number 1-10?\n');
-process.stdin.on('data', data => {
-  let guess = data;
-  process.stdout.write(`Your guess was ${guess}`);
-  if (guess == num) {
-    process.stdout.write(`Correct you guessed ${num}\n`);
-    process.exit();
-  }
-  else {
-    process.stdout.write(`Wrong Guess of ${guess}`);
-  }
+process.stdin.on('data', (data) => {
+    const guess = data;
+    process.stdout.write(`Your guess was ${guess}`);
+    if (guess == num) {
+        process.stdout.write(`Correct you guessed ${num}\n`);
+        process.exit();
+    } else {
+        process.stdout.write(`Wrong Guess of ${guess}`);
+    }
 });
